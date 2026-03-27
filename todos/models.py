a5d73031f9f3e92c1a7cbce693ad7b2c2dd59a06
Igornovic21 <kamguefokoigor@gmail.com>
2026-03-27 14:15:21 -0400

    feat: add todos app, models, apis, views logic

---
+++ todos/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+
+from globals.models.timestampable import TimestampMixin
+
+
+class Todo(TimestampMixin, models.Model):
+    class Status(models.TextChoices):
+        PENDING = 'pending', 'Pending'
+        IN_PROGRESS = 'in_progress', 'In Progress'
+        DONE = 'done', 'Done'
+
+    title = models.CharField(max_length=255)
+    status = models.CharField(
+        max_length=20,
+        choices=Status.choices,
+        default=Status.PENDING,
+    )
+    note = models.ForeignKey(
+        'notes.Note',
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+    )
+
+    class Meta:
+        ordering = ['-updated_at']
+
+    def __str__(self):
+        return self.title
---
+++ todos/views.py
@@ -0,0 +1,9 @@
+from rest_framework import viewsets
+
+from todos.models import Todo
+from todos.serializers import TodoSerializer
+
+
+class TodoViewSet(viewsets.ModelViewSet):
+    queryset = Todo.objects.select_related('note')
+    serializer_class = TodoSerializer
